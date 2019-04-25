2082bff0223cc102d3150f9d26119c8530d11027
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-25 19:46:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,9 @@
 def using_push(array, string)
-  countries_in_western_africa = ["Somalia", "Kenya", "Ethiopia"]
-  updated_array = "Niger"
-  countries_in_western_africa.push(updated_array)
+  array.push(string)
 end
 
 def using_unshift(array, string)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  array.unshift(string)
 end
 
 def using_pop(array)
