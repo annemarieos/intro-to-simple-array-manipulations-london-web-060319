c8e493b8616c05081e977a2af83d3db96173d151
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-25 20:05:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,4 +24,20 @@ end
 
 def using_concat(first_array, second_array)
   first_array.concat(second_array)
-end 
+end
+
+def using_insert(array, new_element)
+  array.insert(new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
