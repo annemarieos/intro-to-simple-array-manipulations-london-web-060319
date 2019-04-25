64b2cf641aec8eef7651a37e0b988cfe4f5fbafc
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-25 19:23:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(array, string)
   countries_in_western_africa = ["Somalia", "Kenya", "Ethiopia"]
-  updated_array = "Tanzania"
+  updated_array = "Niger"
   countries_in_western_africa.push(updated_array)
 end 
