9f593fc483d041ddc70835f91dd31125119e80b9
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-25 19:51:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,4 +16,12 @@ end
 
 def using_shift(array)
   array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(first_array, second_array)
+  first_array.concat(second_array)
 end 
