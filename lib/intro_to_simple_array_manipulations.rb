0493d553b7714d9133f7e94a4c03e34580629b6c
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-25 19:28:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,4 @@ end
 def using_pop(array)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   deleted_string = great_hits_of_the_nineties.pop
+end 
